e6506842d305f3c15cd625a447af2d4f4335442a
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-27 12:42:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return (year - this.startDate.getUTCFullYear())
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let vertical =  Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    let horBeg = eastWest.indexOf(this.beginningLocation.horizontal);
+    let horEnd = eastWest.indexOf(this.endingLocation.horizontal);
+    let horizontal = Math.abs(horBeg - horEnd)
+    
+    return horizontal + vertical
+  }
+  
+  estimatedTime(peak_hours) {
+    
+    if(peak_hours) {
+      return this.blocksTravelled()/2 
+    } else {
+      return this.blocksTravelled()/3 
+    }
+  }
+}
